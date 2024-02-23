08df9c17372f0beb14980abb234c2737f30286ca
Jean_cyifuzo <jeancyifuzodamas@gmail.com>
2024-02-23 15:21:49 +0200

    portifolio with next js

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>LOGO</span>
+        <p className="text-slate-400">
+          {" "}
+          © {new Date().getFullYear()} Jean Portfolio, All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
