925f580fdb8eb305efb935886c8b3a01c57d8765
Jean_cyifuzo <jeancyifuzodamas@gmail.com>
2024-02-28 20:53:34 +0200

    new change

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,13 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/jeandamascyifuzo" target="_blank">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link
+            href="https://www.linkedin.com/in/damascene-cyifuzo-9330ba17a/"
+            target="_blank"
+          >
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
