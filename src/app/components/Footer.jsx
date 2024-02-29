0098987a932d320cf420141c150869f98cd59e9d
Jean_cyifuzo <jeancyifuzodamas@gmail.com>
2024-02-29 12:11:56 +0200

    new change

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Footer = () => {
-  const data = [];
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
