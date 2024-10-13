2f18e048341e7bad21647e0e63a7af040b63961d
Jean_cyifuzo <jeancyifuzodamas@gmail.com>
2024-10-13 17:37:44 +0200

    New updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,18 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'portifolio',
+  title: 'Jean Cyifuzo-portifolio',
   description: 'Jean Cyifuzo portifolio',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/my-logo.png" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
